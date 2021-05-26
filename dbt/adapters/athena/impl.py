e281e740863738932656f81e10fce6b49cb47dca
Jordan Hamill <jordan.hamill@zoopla.co.uk>
2021-05-26 13:57:35 +0100

    WIP delete partitions based on query

--- dbt/adapters/athena/impl.py
+++ dbt/adapters/athena/impl.py
@@ -1,5 +1,9 @@
+from datetime import date, datetime
+from typing import Any, Dict, List, Tuple
+from urllib.parse import urlparse
 from uuid import uuid4
 import agate
+import boto3
 
 from dbt.adapters.base import available
 from dbt.adapters.sql import SQLAdapter
@@ -20,21 +24,121 @@ class AthenaAdapter(SQLAdapter):
         return "string"
 
     @classmethod
-    def convert_number_type(
-        cls, agate_table: agate.Table, col_idx: int
-    ) -> str:
+    def convert_number_type(cls, agate_table: agate.Table, col_idx: int) -> str:
         decimals = agate_table.aggregate(agate.MaxPrecision(col_idx))
         return "double" if decimals else "integer"
 
     @classmethod
-    def convert_datetime_type(
-            cls, agate_table: agate.Table, col_idx: int
-    ) -> str:
+    def convert_datetime_type(cls, agate_table: agate.Table, col_idx: int) -> str:
         return "timestamp"
 
     @available
     def s3_uuid_table_location(self):
+        base_url = self._s3_base_location()
+        return f"{base_url}/tables/{str(uuid4())}/"
+
+    @available
+    def s3_table_location(self, table_name: str) -> str:
+        return f"{self._s3_base_location()}/{table_name}/"
+
+    @available
+    def s3_list_objects_in_partitions(
+        self,
+        relation: AthenaRelation,
+        partitions: Dict[str, Any],
+        allow_no_parttions: bool = False,
+    ) -> List[str]:
+        """Partitions is a dict of partition column name to value mapping.
+        If value is a `dict` you can use an `IN` or `BETWEEN` condition.
+        ```
+        {
+            "date": {
+                "between": {
+                    "start": "2021-05-01",
+                    "end": "2021-05-31",
+                }
+            }
+        }
+        {
+            "date": {
+                "in": ["2021-05-01", "2021-05-02"]
+            }
+        }
+        """
+        query = f'select "$path" as s3_url from {relation.render()}'
+        conditions = []
+        for partition_column, partition_value in partitions.items():
+            if isinstance(partition_value, dict):
+                if "between" in partition_value:
+                    between = partition_value["between"]
+                    start = self._terrible_basic_sql_escape(between["start"])
+                    end = self._terrible_basic_sql_escape(between["end"])
+                    conditions.append(f"{partition_column} between {start} and {end}")
+                elif "in" in partition_value:
+                    comma_separated = ", ".join(
+                        (
+                            self._terrible_basic_sql_escape(value)
+                            for value in partition_value["in"]
+                        )
+                    )
+                conditions.append(f"{partition_column} in ({comma_separated})")
+            else:
+                conditions.append(
+                    f"{partition_column} = {self._terrible_basic_sql_escape(partition_value)}"
+                )
+
+        where = " where " + (" and ".join(conditions))
+        if conditions:
+            query += where
+        elif not allow_no_parttions:
+            raise ValueError("Must provide `partitions`")
+
+        _, table = self.execute(sql=query, auto_begin=True, fetch=True)
+        s3_urls = [row["s3_url"] for row in table.rows]
+        return s3_urls
+
+    @available
+    def s3_delete_objects(self, s3_urls: List[str]) -> None:
+        if not s3_urls:
+            return
+
+        if len(s3_urls) > 1000:
+            assert False, "Need to allow deleting more than 1000 parquet files"
+
+        bucket_and_keys = [self._s3_bucket_and_object_key(url) for url in s3_urls]
+        bucket = bucket_and_keys[0][0]
+
+        s3 = boto3.client("s3")
+        s3.delete_objects(
+            Bucket=bucket,
+            Delete={
+                "Objects": [{"Key": key} for _, key in bucket_and_keys],
+                "Quiet": False,
+            },
+        )
+
+    def _terrible_basic_sql_escape(self, value: Any) -> str:
+        if isinstance(value, str):
+            return f"'{value}'"
+        if isinstance(value, (date, datetime)):
+            return f"'{value}'"
+        return str(value)
+
+    def _s3_base_location(self):
         conn = self.connections.get_thread_connection()
         client = conn.handle
 
-        return f"{client.s3_staging_dir}tables/{str(uuid4())}/"
+        url = client.s3_staging_dir.rstrip("/")
+        return url
+
+    def _s3_bucket_and_object_key(self, s3_url) -> Tuple[str, str]:
+        parsed = urlparse(s3_url, allow_fragments=False)
+        return parsed.netloc, parsed.path.lstrip("/")
+
+    def _s3_delete_objects(self, bucket: str, key_prefix: str) -> None:
+        if not key_prefix:
+            return
+
+        s3 = boto3.resource("s3")
+        bucket = s3.Bucket(bucket)
+        bucket.objects.filter(Prefix=key_prefix).delete()
---
+++ dbt/include/athena/macros/materializations/idempotent_incremental.sql
@@ -0,0 +1,63 @@
+{% materialization idempotent_incremental, adapter='athena' -%}
+
+  {% set unique_key = config.get('unique_key') %}
+
+  {% set overwrite_msg -%}
+    Athena adapter does not support 'unique_key'
+  {%- endset %}
+  {% if unique_key is not none %}
+    {% do exceptions.raise_compiler_error(overwrite_msg) %}
+  {% endif %}
+
+  {% set target_relation = this.incorporate(type='table') %}
+  {% set existing_relation = load_relation(this) %}
+  {% set tmp_relation = make_temp_relation(this) %}
+
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+  -- `BEGIN` happens here:
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+  {% set to_drop = [] %}
+  {% if existing_relation is none %}
+      {% set build_sql = create_table_as(False, target_relation, sql) %}
+  {% elif existing_relation.is_view or should_full_refresh() %}
+      {% do adapter.drop_relation(existing_relation) %}
+      {% set build_sql = create_table_as(False, target_relation, sql) %}
+  {% else %}
+      {% set overwrite_partitions_where = config.require('overwrite_partitions_where') %}
+      {% set existing_s3_urls = adapter.s3_list_objects_in_partitions(existing_relation, overwrite_partitions_where) %}
+      {{ log(existing_s3_urls, info=true) }}
+      {% do adapter.s3_delete_objects(existing_s3_urls) %}
+
+      {% set tmp_relation = make_temp_relation(target_relation) %}
+      {% do run_query(create_table_as(True, tmp_relation, sql)) %}
+      {% set build_sql = incremental_insert(tmp_relation, target_relation) %}
+      {% do to_drop.append(tmp_relation) %}
+  {% endif %}
+
+  {% call statement("main") %}
+      {{ build_sql }}
+  {% endcall %}
+
+  -- set table properties
+  {% if not to_drop %}
+    {{ set_table_classification(target_relation, 'parquet') }}
+  {% endif %}
+
+  {% do persist_docs(target_relation, model) %}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  -- `COMMIT` happens here
+  {% do adapter.commit() %}
+
+  {% for rel in to_drop %}
+      {% do adapter.drop_relation(rel) %}
+  {% endfor %}
+
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+  {{ return({'relations': [target_relation]}) }}
+
+{%- endmaterialization %}
